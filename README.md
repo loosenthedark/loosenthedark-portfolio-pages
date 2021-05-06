b2dbc2acd4ae285418abd6198824d6e2f6f0136e
Paul Harrington <paulharrington05@gmail.com>
2021-05-06 20:09:09 +0000

    Finish styling all remaining elements including Contact form submit modal redirect on mobile

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## References & Resources
+
 * 'Undulating negative space' photo by <a href="https://unsplash.com/@vanschneider?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tobias van Schneider</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> compressed using [TinyJPG](https://tinyjpg.com/) and used with permission
 
 * loosenthedark logo (`.navbar-brand > img`) designed using [Canva](https://t.co/4M8BxU5BYW?amp=1) and a customised [Font Awesome SVG icon](https://fontawesome.com/icons/laptop-code?style=solid)
@@ -5,3 +7,5 @@
 * Several Font Awesome icons have been modified for use in the site's custom `.card-profile-img` components - this is permitted under [the terms of Font Awesome's user licence](https://fontawesome.com/license)
 
 * Boilerplate for the 'scroll back to top' `button` adpated from [this example](https://bbbootstrap.com/snippets/simple-back-top-smooth-scroll-17111555)
+
+* ['Bootstrap 4: Trigger page redirect after modal is hidden'](https://elennion.wordpress.com/2018/10/04/bootstrap-4-trigger-page-redirect-after-modal-is-hidden/)
--- assets/js/email.js
+++ assets/js/email.js
@@ -1,36 +1,151 @@
-window.onload = function() {
-  document.getElementById('input-email').value = '';
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
+window.onload = function () {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
       event.preventDefault();
-
-      function isEmail(email) {
-          var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-          return regex.test(email);
+      function isName(name) {
+        var regex = /^[a-zA-Z\s]*$/;
+        return regex.test(name);
       }
-      var emailAlert = '';
-      var isEmailValid = document.getElementById('input-email').value;
-      if (isEmailValid == '') {
-          emailAlert += "<div class='alert alert-warning text-center alert-contact' role='alert'><span class='icon icon-warning'></span><br><p class='alert-para'>You forgot to enter your email address!</p><p class='alert-para'>Please try again.</p></div>";
-          document.getElementById('error-message-email').innerHTML = emailAlert;
+      function isEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+      }
+      function isNameTooShort(name) {
+        if (name.length < 2) {
           return false;
-      } else if (!isEmail(isEmailValid)) {
-          emailAlert += "<div class='alert alert-warning text-center alert-contact' role='alert'><span class='icon icon-warning'></span><br><p class='alert-para'>Please enter a <span class='underline'>valid</span> email address!</p><p class='alert-para'>e.g. <span class='emphasise'>yourname@gmail.com</span></p></div>";
-          document.getElementById('error-message-email').innerHTML = emailAlert;
+        } else {
+          return true;
+        }
+      }
+      function isNameTooLong(name) {
+        if (name.length > 30) {
           return false;
+        } else {
+          return true;
+        }
+      }
+      function isEmailTooShort(email) {
+        if (email.length < 7) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+      function isEmailTooLong(email) {
+        if (email.length > 100) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+      function isMessageTooShort(message) {
+        if (message.length < 11) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+      function isMessageTooLong(message) {
+        if (message.length > 1000) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+      var nameAlert = "";
+      var emailAlert = "";
+      var messageAlert = "";
+      var isNameValid = document.getElementById("name").value;
+      var isEmailValid = document.getElementById("email").value;
+      var isMessageValid = document.getElementById("message").value;
+      // no empty input fields validation
+      if (isNameValid == "" || isEmailValid == "" || isMessageValid == "") {
+        nameAlert +=
+          "<div class='alert alert-warning text-center alert-contact alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para'>Please fill out all fields!</p></div>";
+        document.getElementById("error-message").innerHTML = nameAlert;
+        return false;
+      }
+      // name validation
+      if (!isName(isNameValid)) {
+        nameAlert +=
+          "<div class='alert alert-warning text-center alert-contact alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para'>Please enter a <span class='underline'>valid</span> name!</p>";
+        document.getElementById("error-message").innerHTML = nameAlert;
+        return false;
+      } else if (!isNameTooShort(isNameValid)) {
+        nameAlert +=
+          "<div class='alert alert-warning text-center alert-contact' alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para'>The name you have entered is too short! Please try again.</p>";
+        document.getElementById("error-message").innerHTML = nameAlert;
+        return false;
+      } else if (!isNameTooLong(isNameValid)) {
+        nameAlert +=
+          "<div class='alert alert-warning text-center alert-contact' alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para'>The name you have entered is too long! Please try again (max. 30 characters)</p>";
+        document.getElementById("error-message").innerHTML = nameAlert;
+        return false;
+      }
+      // email validation
+      if (!isEmail(isEmailValid)) {
+        emailAlert +=
+          "<div class='alert alert-warning text-center alert-contact' alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para alert-para-inner'>Please enter a <span class='underline'>valid</span> email address!</p><p class='alert-para'>e.g. <span class='emphasise'>yourname@gmail.com</span></p></div>";
+        document.getElementById("error-message").innerHTML = emailAlert;
+        return false;
+      } else if (!isEmailTooShort(isEmailValid)) {
+        emailAlert +=
+          "<div class='alert alert-warning text-center alert-contact' alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para'>The email you have entered is too short! Please try again.</p>";
+        document.getElementById("error-message").innerHTML = emailAlert;
+        return false;
+      } else if (!isEmailTooLong(isEmailValid)) {
+        emailAlert +=
+          "<div class='alert alert-warning text-center alert-contact' alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para'>The email you have entered is too long! Please try again (max. 100 characters)</p>";
+        document.getElementById("error-message").innerHTML = emailAlert;
+        return false;
+      }
+      // message validation
+      if (!isMessageTooShort(isMessageValid)) {
+        messageAlert +=
+          "<div class='alert alert-warning text-center alert-contact' alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para'>Your message is too short! Please try again.</p>";
+        document.getElementById("error-message").innerHTML = messageAlert;
+        return false;
+      } else if (!isMessageTooLong(isMessageValid)) {
+        messageAlert +=
+          "<div class='alert alert-warning text-center alert-contact' alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para'>Your message is too long! Please try again (max. 1,000 characters)</p>";
+        document.getElementById("error-message").innerHTML = messageAlert;
+        return false;
       }
       // generate a five digit number for the contact-number variable
-      this.contact-number.value = Math.random() * 100000;
+      this.contact_number.value = Math.random() * 100000;
       // these IDs from the previous steps
-      emailjs.sendForm('gmail', 'contact_form', this)
-          .then(function(response) {
-              document.getElementById('error-message-email').innerHTML = '';
-              document.getElementById('input-email').value = '';
-              $('#modal-submit').modal();
-          }, function(error) {
-              setTimeout(() => {
-                  document.getElementById('error-message-email').innerHTML = "<div class='alert alert-danger text-center alert-contact' role='alert'><span class='icon icon-warning'></span><br><p class='alert-para'>We're sorry, but something's gone wrong.</p><p class='alert-para'>Please refresh the page and try again!</p></div>";
-                  document.getElementById('input-email').value = '';
-              }, 3000);
-          });
-  });
+      emailjs.sendForm("gmail", "ltd_contact_form", this).then(
+        function (response) {
+          document.getElementById("error-message").innerHTML = "";
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          $("#modal-contact").modal();
+        },
+        function (error) {
+          setTimeout(() => {
+            document.getElementById("error-message").innerHTML =
+              "<div class='alert alert-danger text-center alert-contact' alert-dismissible fade show' role='alert'><button type='button' class='p-0 close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><span class='icon icon-warning'></span><br><p class='alert-para alert-para-inner'>We're sorry, but something's gone wrong...</p><p class='alert-para'>Please refresh the page and try again!</p></div>";
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+          }, 3000);
+        }
+      );
+    });
 };
+
+$(document).ready(function () {
+  $(document).on("shown.bs.modal", "#modal-contact", function () {
+    setTimeout(function () {
+      $("#modal-contact").modal("hide");
+    }, 5000);
+  });
+  $(document).on("hidden.bs.modal", "#modal-contact", function () {
+    window.location.href = "index.html";
+  });
+});
